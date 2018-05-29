1c8725a1c7e4415825a05e14811212031eea2f26
Mo Battah <mohhandb@hotmail.com>
2018-05-29 16:43:00 -0500

    Update testjsonsend.py

--- testjsonsend.py
+++ testjsonsend.py
@@ -1,6 +1,5 @@
 
 import sys
-import json
 import pyodbc
 from flask import Flask, request, abort
 
@@ -12,20 +11,26 @@ def main():
     print("webhook"); sys.stdout.flush()
     if request.method == 'POST':
         stdstatement("check_id")
+        stdstatement("check_type")
+        stdstatement("previous_state")
         return '', 200
     else:
         abort(400)
 
 
-def stdstatement (columnname):
+def stdstatement(columnname):
     param = request.json[str(columnname)]
     param = str(param)
     stdstatement1 = "INSERT INTO WebHookTestTable ("+columnname+") VALUES("+param+")"
     stdstatement1 = str(stdstatement1)
+    print(stdstatement1)
+    SQLExecute(stdstatement1)
+
+def SQLExecute(executestring):
     conn = pyodbc.connect(r'DSN=TestWebHookDB;UID=mo.battah;PWD=pass')
     conn.autocommit = True
     cursor = conn.cursor()
-    cursor.execute("" + stdstatement1 + "")
+    cursor.execute("" + executestring + "")
 
 
 if __name__ == '__main__':
