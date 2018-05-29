46ba62ad1f0693208c094ed32e0d604159db563b
Mo Battah <mohhandb@hotmail.com>
2018-05-29 15:53:05 -0500

    Update testjsonsend.py

--- testjsonsend.py
+++ testjsonsend.py
@@ -10,25 +10,19 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def main():
     print("webhook"); sys.stdout.flush()
-    conn = pyodbc.connect(r'DSN=TestWebHookDB;UID=user;PWD=password')
+    conn = pyodbc.connect(r'DSN=TestWebHookDB;UID=mo.battah;PWD=password')
     conn.autocommit = True
     if request.method == 'POST':
-        check_id = request.json['check_id'] #get the value
-        #check_id = str(check_id) #sql column is nvarcharmax wont take integers
-        sqlstatement = stdstatement("check_id", check_id)
+        sqlstatement = stdstatement("check_id")
         cursor = conn.cursor()
-        # sqlstatement = "INSERT INTO WebHookTestTable (check_id) VALUES ("+check_id+")"
-        # sqlstatement = str(sqlstatement)
         cursor.execute(""+sqlstatement+"")
-        # check_type = request.json['check_type']
-        #
-
-
         return '', 200
     else:
         abort(400)
 
-def stdstatement (columnname, param):
+
+def stdstatement (columnname):
+    param = request.json[str(columnname)]
     param = str(param)
     stdstatement1 = "INSERT INTO WebHookTestTable ("+columnname+") VALUES("+param+")"
     stdstatement1 = str(stdstatement1)
