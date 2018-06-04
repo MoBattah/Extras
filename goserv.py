bddd26fd28f5cacf7ccc2626eab0f5d2cdb3a5d6
Mo Battah <mohhandb@hotmail.com>
2018-06-04 15:28:23 -0500

    Add files via upload

---
+++ goserv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""WSGI server example"""
+from __future__ import print_function
+from gevent.pywsgi import WSGIServer
+from AWSWebHook import app
+
+def application(env, start_response):
+    if env['PATH_INFO'] == '/':
+        start_response('200 OK', [('Content-Type', 'text/html')])
+        return [b"<b>hello world</b>"]
+
+    start_response('404 Not Found', [('Content-Type', 'text/html')])
+    return [b'<h1>Not Found</h1>']
+
+
+if __name__ == '__main__':
+    print('Serving on 8088...')
+    WSGIServer(('0.0.0.0', 8088), app).serve_forever()
+    if __name__ == '__main__':
+        WSGIServer(('0.0.0.0', 8088), app).serve_forever()
