b05ab11cd4fb5fcc4d5a7f61215e8b05a951b0a2
Mo Battah <mohhandb@hotmail.com>
2018-06-08 15:27:09 -0500

    Update PingdomWebHook.py

--- PingdomWebHook.py
+++ PingdomWebHook.py
@@ -1,29 +1,29 @@
 import sys
 import pyodbc
 from flask import Flask, request, abort
-import logging
+import datetime
 
 app = Flask(__name__)
 
-
+global pingdomlogfile
+pingdomlogfile = "E:\\Web\\PingdomWebHook\\PingdomWebHookLog.log"
 @app.route('/', methods=['POST'])
 def main():
-    logging.basicConfig(filename='C:\\Users\\mo.battah\\Documents\\PingdomAPI.log', level=logging.DEBUG)
+    sys.stdout = open(pingdomlogfile, 'a')  ###LOGGING
+    print("Start: ", str(datetime.datetime.now()).split('.')[0])
     print("Webhook started");
-    #logging.debug(sys.stdout.flush())
-    logging.debug("Webhook started")
     if request.method == 'POST':
-        logging.debug("Recieved POST")
-        logging.debug(request.json)
+        print("Recieved POST")
+        print(request.json)
         gatherParameters()
         return '', 200
     else:
-        logging.debug("Recieved something other than POST")
+        print("Recieved something other than POST.")
         abort(400)
+    sys.stdout.close()  ###LOGGING
     main()
 
 def gatherParameters():
-    logging.debug("gatherParameters")
     print("GatherParameters")
     check_id = fetchParameter("check_id")
     print("Fetch check_id")
@@ -45,21 +45,21 @@ def gatherParameters():
     check_name = fetchParameter("check_name")
     long_description = fetchParameter("long_description")
     description = fetchParameter("description")
-    sqlStatement = "INSERT INTO WebHookTestTable (check_id, check_name, check_type, full_url, hostname, previous_state, current_state, importance_level, state_changed_timpstamp, state_changed_utc_time, long_description, description) " \
+    sqlStatement = "INSERT INTO WebHookTestTable (check_id, check_name, check_type, full_url, hostname, previous_state, current_state, importance_level, state_changed_timestamp, state_changed_utc_time, long_description, description) " \
                    "VALUES(\'"+check_id+"\',\'"+check_name+"\',\'"+check_type+"\',\'"+full_url+"\',\'"+hostname+"\',\'"+previous_state+"\',\'"+current_state+"\',\'"+importance_level+"\',\'"+state_changed_timestamp+"\',\'"+state_changed_utc_time+"\',\'"+long_description+"\',\'"+description+"\')"
     SQLExecute(sqlStatement)
-    logging.debug("Executed "+sqlStatement)
+    print("Executed "+sqlStatement)
 
 
 def fetchParameter(columnname):
-    logging.debug("fetchParameter")
+    print("fetchParameter")
     param = request.json[str(columnname)]
     param = str(param)
     return param
 
 
 def SQLExecute(executestring):
-    logging.debug("SQLExecute")
+    print("SQLExecute")
     conn = pyodbc.connect(
         r'DRIVER={ODBC Driver 17 for SQL Server};'
         r'SERVER=devops01test.database.windows.net;'
@@ -69,10 +69,9 @@ def SQLExecute(executestring):
     )
     conn.autocommit = True
     cursor = conn.cursor()
-    logging.debug(executestring)
     print(executestring)
     cursor.execute("" + executestring + "")
-    logging.debug("SQL executed")
+    print("SQL executed")
 
 
 if __name__ == '__main__':
