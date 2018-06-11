1a56a470db4d92005e1408d53b447a915d88a060
Mo Battah <mohhandb@hotmail.com>
2018-06-11 13:48:19 -0500

    Update SQLResumeExtracter.py

--- ResumeExtraction/SQLResumeExtracter.py
+++ ResumeExtraction/SQLResumeExtracter.py
@@ -1,3 +1,9 @@
 import os
-astring = "BCP \'SELECT TOP (1) [FileContent] FROM [Recruitment].[dbo].[file_docx_vw] WHERE [ID] = 72' queryout C:\\Users\\mo.battah\\Documents\\filey2.docx -n -T  -S defrsqlwp005.vistex.local"
-os.system('powershell.exe ' + astring)
+
+for x in range(1,1674):
+    counter = str(x)
+    astring = "BCP \'SELECT TOP (1) [FileContent] FROM [Recruitment].[dbo].[file_docx_vw] WHERE [ID] = " + counter + "' queryout C:\\Users\\mo.battah\\Documents\\resume"+ counter + ".docx -n -T  -S defrsqlwp005.vistex.local"
+    astring = str(astring)
+    print(astring)
+    os.system('powershell.exe ' + astring)
+
