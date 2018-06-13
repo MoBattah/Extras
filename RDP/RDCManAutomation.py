9a4754f36db6d9aa3c42aaf927875fb8c7d949f7
Mo Battah <mohhandb@hotmail.com>
2018-06-13 15:01:32 -0500

    Update RDCManAutomation.py

--- RDP/RDCManAutomation.py
+++ RDP/RDCManAutomation.py
@@ -11,7 +11,8 @@ def main():
     uncat = []
     ops = []
     deprecated = []
-    createlist(cursor)
+    demo, development, stage, uncat, production, ops, integration, deprecated = createlist(cursor)
+    createstring(demo, development, stage, uncat, production, ops, integration, deprecated)
 
 def createlist(cursor):
     switchstmt = {
@@ -26,18 +27,55 @@ def createlist(cursor):
     }
     for item in cursor:
         switchstmt[item[1]](item[3])
-    print(demo)
+    return demo, development, stage, uncat, production, ops, integration, deprecated
 
-def createstring():
+def createstring(demo, development, stage, uncat, production, ops, integration, deprecated):
     head = "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
     RDCManopen = "<RDCMan programVersion=\"2.7\" schemaVersion=\"3\"><file><credentialsProfiles /><properties><expanded>True</expanded><name>aformat</name></properties>"
-    OperationsGroup = "<group><properties><expanded>True</expanded><name>Operations</name></properties><server><properties><name>mwm.vistex.com</name></properties></server><server><properties><name>NC01IISWSS004.hawkeyeww.com</name></properties></server></group>"
-    DevGroup = "<group> <properties> <expanded>True</expanded> <name>Development</name> </properties> <server> <properties> <name>NC01IISWPS004.hawkeyeww.com</name> </properties> </server> </group>"
-    ProdGroup = "<group> <properties> <expanded>True</expanded> <name>Production</name> </properties> <server> <properties> <name>nc01IISWPS005.hawkeyeww.com</name> </properties> </server> </group>"
+    OperationsGroup = "<group> <properties> <expanded>True</expanded> <name>Operations</name> </properties>"
+    DevGroup = "<group> <properties> <expanded>True</expanded> <name>Development</name> </properties>"
+    StageGroup = "<group> <properties> <expanded>True</expanded> <name>Stage</name> </properties>"
+    ProdGroup = "<group> <properties> <expanded>True</expanded> <name>Production</name> </properties>"
+    DeprecatedGroup = "<group> <properties> <expanded>True</expanded> <name>DEPRECATED</name> </properties>"
     Ending = "</file> <connected /> <favorites /> <recentlyUsed /> </RDCMan>"
+    UncatGroup = "<group> <properties> <expanded>True</expanded> <name>Uncategorized</name> </properties>"
+    IntegrationGroup = "<group> <properties> <expanded>True</expanded> <name>Integration</name> </properties>"
+    DemoGroup = "<group> <properties> <expanded>True</expanded> <name>Demo</name> </properties>"
+    development = list(set(development))
+    demo = list(set(demo))
+    stage = list(set(stage))
+    uncat = list(set(uncat))
+    production = list(set(production))
+    ops = list(set(ops))
+    integration = list(set(integration))
+    deprecated = list(set(deprecated))
 
-    fp = open("C:\\Users\\mo.battah\\hello.rdg", 'w+')
-    fp.write(head + RDCManopen + OperationsGroup + DevGroup + ProdGroup + Ending)
+
+    for item in production:
+        ProdGroup = ProdGroup + "<server> <properties> <name>" + item + "</name> </properties> </server>"
+    ProdGroup = ProdGroup + "</group>"
+    for item in development:
+        DevGroup = DevGroup + "<server> <properties> <name>" + item + "</name> </properties> </server>"
+    DevGroup = DevGroup + "</group>"
+    for item in stage:
+        StageGroup = StageGroup + "<server> <properties> <name>" + item + "</name> </properties> </server>"
+    StageGroup = StageGroup + "</group>"
+    for item in demo:
+        DemoGroup = DemoGroup + "<server> <properties> <name>" + item + "</name> </properties> </server>"
+    DemoGroup = DemoGroup + "</group>"
+    for item in uncat:
+        UncatGroup = UncatGroup + "<server> <properties> <name>" + item + "</name> </properties> </server>"
+    UncatGroup = UncatGroup + "</group>"
+    for item in integration:
+        IntegrationGroup = IntegrationGroup + "<server> <properties> <name>" + item + "</name> </properties> </server>"
+    IntegrationGroup = IntegrationGroup + "</group>"
+    for item in deprecated:
+        DeprecatedGroup = DeprecatedGroup + "<server> <properties> <name>" + item + "</name> </properties> </server>"
+    DeprecatedGroup = DeprecatedGroup + "</group>"
+
+
+    fp = open("C:\\Users\\mo.battah\\final.rdg", 'w+')
+    fp.write(head + RDCManopen + DevGroup + StageGroup + ProdGroup + DemoGroup + UncatGroup + IntegrationGroup + DeprecatedGroup + Ending)
     fp.close()
 
 
