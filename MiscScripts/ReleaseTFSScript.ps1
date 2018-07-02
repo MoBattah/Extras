0e008b6d8cf1de92c698a00bc45301546280ab17
Mo Battah <mohhandb@hotmail.com>
2018-07-02 11:29:00 -0500

    Update ReleaseTFSScript.ps1

--- MiscScripts/ReleaseTFSScript.ps1
+++ MiscScripts/ReleaseTFSScript.ps1
@@ -1,16 +1,27 @@
 param(
-[string]$copysource,
-[string]$copydest,
-#[string]$environment
+[string]$copysource, #source of copy
+[string]$copydest, #destination of copy
+[string]$webconfigfile #config file to be deployed
 )
 
+$dirlist = Get-ChildItem -Path $copysource
 $jsonpath = $copydest + "\Json\TenantsList.json"
+$webconfigpath = $copysource + $webconfigfile
+$webconfigdest = $copydest + "\Web.config"
 
 #if the TenantsList json file exists,it excludes it from the copy operation. 
 if (Test-Path $jsonpath) 
-{Get-ChildItem -Path $copysource |
+{$dirlist |
   % {
   Copy-Item $_.fullname $copydest -Recurse -Force -Exclude @("TenantsList.json", "Json", "Web*config") 
 }} 
 Else 
-{Copy-Item -Path $copysource* -Recurse -Destination $copydest -Force}
+{Copy-Item -Path $copysource* -Recurse -Destination $copydest -Force -Exclude @("Web*config")} #refactor beginning of this
+
+if (Test-Path -Path $webconfigpath) {
+    Copy-Item -Path $webconfigpath -Destination $webconfigdest -Force
+    Write-Host $webconfigdest "copied"
+  }
+  else {
+  Write-Host "Web.config not replaced"
+  }
