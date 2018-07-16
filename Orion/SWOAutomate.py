8a263b89ecd1cd1b7e62b1b7bdb309700c80b31e
Mo Battah <mohhandb@hotmail.com>
2018-07-16 09:27:36 -0500

    Update SWOAutomate.py

--- Orion/SWOAutomate.py
+++ Orion/SWOAutomate.py
@@ -1,11 +1,20 @@
 import textwrap
 import pyodbc
+import sys
+import datetime
+
+global logfile
+logfile = "C:\\Users\\mo.battah\\orionlogfile"
 
 def main():
 
+    sys.stdout = open(logfile, 'w+')
+    print("Start: ", str(datetime.datetime.now()).split('.')[0])
+    cursor = SQLExecute("SELECT [VolumeType], [FullName],[VolumePercentUsed] FROM [SW_ORION_PROD].[dbo].[Volumes] WHERE VolumePercentUsed >= '90'")
+    getavg(cursor)
     cursor = SQLExecute("SELECT [VolumeType], [FullName],[VolumePercentUsed] FROM [SW_ORION_PROD].[dbo].[Volumes] WHERE VolumePercentUsed >= '90'")
     shrinkSQLlogs(cursor)
-    #query_string = create_query_string(r"C:\Users\mo.battah\Desktop\Maintenance_ShrinkAllLogFiles.sql")
+    sys.stdout.close()
 
 
 def SQLExecute(executestring):
@@ -32,20 +41,29 @@ def shrinkSQLlogs(cursor):
     print(sqlboxes, len(sqlboxes))#for logging
     sqlboxes = set(sqlboxes)
     print(sqlboxes, len(sqlboxes))
+
     for row in sqlboxes:
-        print("Going to this row:", row)
-        connecttoremote(row)
+        print("Going to this server:", row)
+        try: connecttoremote(row)
+        except pyodbc.OperationalError as e:
+            print("Some operational error occurred here, probably did not connect. ")
+            print(e)
 
 
 
 
 
-def create_query_string(sql_full_path):
+def create_query_string(sql_full_path, cursor):
     with open(sql_full_path, 'r') as f_in:
         lines = f_in.read()
 
         query_string = textwrap.dedent("""{}""".format(lines))
-        return query_string
+        cursor.execute(query_string)
+        try:
+            for row in cursor:
+                print(row)
+        except pyodbc.ProgrammingError as e:
+            print(e)
 
 
 def connecttoremote(hostname):
@@ -59,7 +77,17 @@ def connecttoremote(hostname):
     cursor.execute("SELECT * FROM sys.databases")
     for row in cursor:
         print(row)
-    query_string = create_query_string(r"C:\Users\mo.battah\Desktop\Maintenance_ShrinkAllLogFiles.sql")
-    cursor.execute(query_string)
+    create_query_string(r"C:\Users\mo.battah\Desktop\Reporting_DatabaseFileSize.sql", cursor)
+    create_query_string(r"C:\Users\mo.battah\Desktop\Maintenance_ShrinkAllLogFiles.sql", cursor)
+    create_query_string(r"C:\Users\mo.battah\Desktop\Maintenance_ShrinkAllDataFiles.sql", cursor)
+    create_query_string(r"C:\Users\mo.battah\Desktop\Reporting_DatabaseFileSize.sql", cursor)
 
+def getavg(cursor):
+    count = 0
+    total = 0
+    for row in cursor:
+        total = row[2] + total
+        count = count + 1
+    avg = total / count
+    print("The average disk load: ", avg, "%")
 main()
