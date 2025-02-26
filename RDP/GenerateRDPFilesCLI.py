5406f273627c5a237650ad343f596db6e2038495
Mo Battah <devbattah@gmail.com>
2025-02-26 00:02:48 -0800

    Update GenerateRDPFilesCLI.py

--- RDP/GenerateRDPFilesCLI.py
+++ RDP/GenerateRDPFilesCLI.py
@@ -1,78 +1,78 @@
-#Command-line application for automatically generating RDP files.
-#Change aws_profile before using.
-
 import click
 import boto3
 import os
 
-global profile
-aws_profile = 'allpower'
+AWS_PROFILE = 'allpower'
+RDP_DIR = 'RDPFiles'
+RDCMAN_FILE = 'AWS.rdg'
 
 @click.group()
 def main():
     """
-    Simple CLI for creating remote desktop configuration files
+    Simple CLI for creating remote desktop configuration files.
     """
     pass
 
 @main.command()
 def mac():
     """Creates Mac-compatible RDP files"""
-    build_dictionary = get_instance_information()
-    create_mac_rdp_files(build_dictionary)
-    click.echo("Created RDP files for Mac in a folder named RDP Files.")
+    instances = get_instance_information()
+    create_mac_rdp_files(instances)
+    click.echo(f"Created RDP files for Mac in the '{RDP_DIR}' folder.")
 
 @main.command()
 def rdcman():
     """Creates Remote Desktop Connection Manager (.rdg) File"""
-    build_dictionary = get_instance_information()
-    create_rdcman_config(build_dictionary)
-    click.echo("Created AWS.rdg Please import to Remote Desktop Connection Manager.")
-
+    instances = get_instance_information()
+    create_rdcman_config(instances)
+    click.echo(f"Created {RDCMAN_FILE}. Please import it into Remote Desktop Connection Manager.")
 
 def get_instance_information():
-    instance_dictionary = {}
-    session = boto3.Session(profile_name=aws_profile)
-    ec = session.client('ec2')
-    reservations = ec.describe_instances()
-    for r in reservations['Reservations']:
-        for i in r['Instances']:
-            instance_id = i['InstanceId']
-            try:
-                if i['Platform'] == 'windows':
-                    for t in i['Tags']:
-                        if t['Key'] == 'Name':
-                            instance_name = t['Value']
-                            ip_address = i['PrivateIpAddress']
-                            instance_dictionary[instance_name] = ip_address
-            except KeyError:
-                pass
-    return instance_dictionary
-
-def create_rdcman_config(build_dictionary):
-    all_servers_string = ""
-    for i in build_dictionary:
-        server_string = "<server> <properties> <name>" + build_dictionary[i] + "</name> <displayName>" + i + "</displayName> </properties> </server>"
-        all_servers_string = all_servers_string + server_string
-
-    RDCManopen = "<?xml version=\"1.0\" encoding=\"utf-8\"?><RDCMan programVersion=\"2.7\" schemaVersion=\"3\"><file><credentialsProfiles /><properties><expanded>True</expanded><name>Servers</name></properties>"
-    Ending = "</group> </file> <connected /> <favorites /> <recentlyUsed /> </RDCMan>"
-    DevGroup = "<group> <properties> <expanded>True</expanded> <name>AWS Windows Servers</name> </properties>"
-    file = open("AWS.rdg", "w+")
-    file.write(RDCManopen + DevGroup + all_servers_string + Ending)
-    file.close()
-
-def create_mac_rdp_files(build_dictionary):
-    cwd = os.getcwd()
+    """Fetches Windows EC2 instances and their private IPs."""
+    instances = {}
+    session = boto3.Session(profile_name=AWS_PROFILE)
+    ec2_client = session.client('ec2')
+    
     try:
-        os.mkdir('RDPFiles')
-    except FileExistsError:
-        pass
-    os.chdir('RDPFiles')
-    for i in build_dictionary:
-        filename = i + ".rdp"
-        file = open(filename, 'w+')
-        config_file = """gatewaybrokeringtype:i:0
+        reservations = ec2_client.describe_instances()
+    except Exception as e:
+        click.echo(f"Error fetching instances: {e}", err=True)
+        return instances
+    
+    for reservation in reservations.get('Reservations', []):
+        for instance in reservation.get('Instances', []):
+            if instance.get('Platform') == 'windows':
+                name = next((tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Name'), instance['InstanceId'])
+                ip_address = instance.get('PrivateIpAddress')
+                if ip_address:
+                    instances[name] = ip_address
+    
+    return instances
+
+def create_rdcman_config(instances):
+    """Generates an RDCMan configuration file."""
+    servers = "".join(
+        f"<server><properties><name>{ip}</name><displayName>{name}</displayName></properties></server>"
+        for name, ip in instances.items()
+    )
+    
+    rdcman_content = (
+        '<?xml version="1.0" encoding="utf-8"?>'
+        '<RDCMan programVersion="2.7" schemaVersion="3">'
+        '<file><credentialsProfiles /><properties><expanded>True</expanded><name>Servers</name></properties>'
+        '<group><properties><expanded>True</expanded><name>AWS Windows Servers</name></properties>'
+        f'{servers}'
+        '</group></file><connected /><favorites /><recentlyUsed /></RDCMan>'
+    )
+    
+    with open(RDCMAN_FILE, 'w', encoding='utf-8') as file:
+        file.write(rdcman_content)
+
+def create_mac_rdp_files(instances):
+    """Creates Mac-compatible RDP files."""
+    os.makedirs(RDP_DIR, exist_ok=True)
+    
+    rdp_template = """gatewaybrokeringtype:i:0
 use redirection server name:i:0
 disable themes:i:0
 disable cursor setting:i:0
@@ -111,10 +111,13 @@ gatewayhostname:s:
 camerastoredirect:s:
 drivestoredirect:s:
 session bpp:i:32
-disable wallpaper:i:0""" + "\nfull address:s:" + build_dictionary[i] + "\ngatewayaccesstoken:s:"
-        file.write(config_file)
-        file.close()
-
+disable wallpaper:i:0"""
+    
+    for name, ip in instances.items():
+        rdp_content = f"{rdp_template}\nfull address:s:{ip}\ngatewayaccesstoken:s:"
+        rdp_file_path = os.path.join(RDP_DIR, f"{name}.rdp")
+        with open(rdp_file_path, 'w', encoding='utf-8') as file:
+            file.write(rdp_content)
 
 if __name__ == "__main__":
     main()
